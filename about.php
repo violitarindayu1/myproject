99db9d72f26fdcffdd69db6b512bd811821719b7
violitarindayu1 <157269702+violitarindayu1@users.noreply.github.com>
2024-01-21 14:09:12 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,42 @@
+<?php include "header.php";?>
+		<!-- Awal Page -->
+		<div class="container">
+		<!-- Awal baris -->
+		<div class="row">
+			<div class="col-md-12"><!-- Awal Kolom Pertama -->
+			<div class="panel panel-default">
+				<div class="panel-body">
+				<h2 style="text-muted"><span class="glyphicon glyphicon-list"></span> TENTANG MASTER HOTEL</h2>
+						<img src="images/hotel1.jpg" class="img-thumbnail img-responsive">
+						<p>MASTER HOTEL adalah hotel mewah berbintang lima yang terletak di pusat kota dengan desain arsitektur modern dan elegan. Fasad bangunan menonjol dengan kombinasi kaca dan logam yang menciptakan tampilan futuristik. <br/><br/>
+						Lobi hotel menyambut tamu dengan suasana yang hangat, dilengkapi dengan ornamen seni kontemporer dan furnitur bergaya. Staf penerima tamu ramah dan profesional, siap memberikan pelayanan prima sejak kedatangan.<br/><br/>
+						Kamar-kamar tamu dirancang dengan selera tinggi, dilengkapi dengan perabotan berkualitas dan sentuhan desain yang memikat. Setiap kamar dilengkapi dengan fasilitas modern seperti televisi layar datar, minibar, dan akses Wi-Fi yang cepat.<br/><br/>
+						Restoran di Master Hotel menyajikan hidangan kelas dunia dengan menu beragam dari masakan lokal hingga internasional. Ruang makan yang elegan menawarkan pemandangan kota yang menakjubkan, menciptakan pengalaman kuliner yang tak terlupakan.<br/><br/>
+						Fasilitas rekreasi meliputi kolam renang bertema spa, pusat kebugaran modern, dan spa eksklusif. Tamu juga dapat mengakses pusat bisnis yang dilengkapi dengan fasilitas pertemuan dan konferensi terkini.<br/><br/>
+						Master Hotel menawarkan pengalaman menginap yang luar biasa dengan perpaduan kemewahan, kenyamanan, dan pelayanan terbaik. Sebagai destinasi ideal untuk wisatawan bisnis maupun liburan, hotel ini menjadikan setiap kunjungan sebagai pengalaman yang tak terlupakan.<br/><br/>
+						FASILTAS YANG TERSEDIA DI MASTER HOTEL:<br/><br/>
+						<ul>
+						<li>Tempat Bermain</li>
+						<li>Kolam Renang</li>
+						<li>Restoran</li>
+						<li>WIFI</li>
+						</ul>
+				</p>
+				</div>
+      </div>
+			</div><!-- Akhir Kolom Pertama -->
+			
+			<div class="col-md-12"><!-- Awal kolom kedua -->
+			<div class="panel panel-default">
+				<div class="panel-body">
+				<h2 style="text-muted"><span class="glyphicon glyphicon-tasks"></span>Info Lainnya</h2>
+				<h4></h4>
+				<img src="images/pembangunan.jpg" class="img-thumbnail img-responsive">
+				<p>Master Hotel didirikan pada tahun 2010 sebagai proyek ambisius untuk membawa konsep kemewahan ke pusat kota. Pemilik MASTER HOTEL adalah<b> VIOLITA</b> Dengan dedikasi terhadap desain modern dan fasilitas terkini, pembangunan hotel ini selesai pada 2012. Sejak saat itu, Master Hotel telah menjadi landmark yang menggambarkan kualitas tinggi dan standar pelayanan unggul.<br/>
+				</div>
+      </div>
+			</div><!-- Akhir Kolom Kedua -->
+		</div><!-- Akhir Baris -->
+		</div><!--  Akhir Page -->
+		
+<?php include "footer.php"; ?>
